0c9b9cb43ba16d0f41d72fccdf5f7750910fd625
Makcimka1477 <makcimka1477@yandex.ru>
2022-06-23 15:14:33 +0300

    headerComp

---
+++ Desktop/React/newtry/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './Header/Header';
+// import Sidebar from './Sidebar/Sidebar'
+// import Content from './Content/Content';
+
+function App() {
+  return (
+    <div className="App">
+        <header>
+          qweqwe123
+        </header>
+
+    </div>
+  );
+}
+
+export default App;
