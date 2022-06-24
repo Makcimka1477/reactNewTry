36c1f515665c451d4bb01fe21e5b9db2f833c8f1
Makcimka1477 <makcimka1477@yandex.ru>
2022-06-24 17:34:27 +0300

    npm node-sass

--- Desktop/React/newtry/src/App.js
+++ Desktop/React/newtry/src/App.js
@@ -1,15 +1,26 @@
-import './App.css';
-import Header from './Header/Header';
+import './App.scss';
+import Header from './components/Header/Header';
 // import Sidebar from './Sidebar/Sidebar'
 // import Content from './Content/Content';
 
+
 function App() {
+  const state = {
+    employees: [
+      {name: 'John C.', salary: 800, premium: false},
+      {name: 'Alex M.', salary: 3000, premium: true},
+      {name: 'Carl W.', salary: 5000, premium: false},
+    ]
+  };
+  const withPremium = state.employees.reduce((acc, n) => (n.premium) ? acc + 1 : acc, 0);
+
   return (
     <div className="App">
-        <header>
-          qweqwe123
-        </header>
+        <Header withPremium={withPremium} employeesCount={state.employees.length}/>
 
+        <i class="fa-solid fa-trash"></i>
+        <i class="fa-regular fa-cookie"></i>
+        <i class="fa-regular fa-star"></i>
     </div>
   );
 }
--- Desktop/React/newtry/src/components/Header/Header.jsx
+++ Desktop/React/newtry/src/components/Header/Header.jsx
@@ -1,6 +1,12 @@
-const Header = () => {
+import css from './header.module.scss'
+
+const Header = ({ employeesCount, withPremium }) => {
     return (
-        <header>Header</header>
+        <header className={css.header}>
+            <h1 className={css.header__title}>Учет сотрудников в компании</h1>
+            <p className={css.header__employeesCount}>Общеее число сотрудников: {employeesCount}</p>
+            <p className={css.header__employeesWithPremium}>Премию получат: {withPremium}</p>
+        </header>
     );
 }
 
